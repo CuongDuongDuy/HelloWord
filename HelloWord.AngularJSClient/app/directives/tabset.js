e4e51902e982c36771841a7cdf8fa51a35e0d2e3
CuongDuongDuy <cuong.duongduy@gmail.com>
2016-04-01 11:51:44 +0100

    #Update tabset directive.

--- HelloWord.AngularJSClient/app/directives/tabset.js
+++ HelloWord.AngularJSClient/app/directives/tabset.js
@@ -1,4 +1,51 @@
 app.directive('tabset', function () {
+    var controller = ['$sce', function($sce) {
+        var self = this;
+        self.tabs = [];
+        self.newTabId = 1;
+        self.selectedTab = undefined;
+
+        function setSelectedTab(tab, addMore) {
+            if (addMore === true) {
+                self.selectedTab = tab;
+                return;
+            }
+            if (self.selectedTab === tab) {
+                var index = self.tabs.indexOf(tab);
+                if (index + 1 === self.tabs.length) {
+                    self.selectedTab = self.tabs[index - 1];
+                } else {
+                    self.selectedTab = self.tabs[index + 1];
+                }
+            }
+        }
+
+        self.tabAdd = function(tab) {
+            if (tab === undefined) {
+                tab = {};
+                tab.content = 'Tab content ' + self.newTabId;
+            }
+            tab.id = 'Tab' + self.newTabId;
+            tab.title = 'Tab ' + self.newTabId;
+            self.tabs.push(tab);
+            setSelectedTab(tab, true);
+            self.newTabId++;
+        };
+
+        self.tabClose = function(selectedTab) {
+            setSelectedTab(selectedTab, false);
+            var index = self.tabs.indexOf(selectedTab);
+            self.tabs.splice(index, 1);
+        };
+
+        self.selectionChanged = function(selectedTab) {
+            self.selectedTab = selectedTab;
+        };
+
+        self.trustAsHtml = function(tabContent) {
+            return $sce.trustAsHtml(tabContent);
+        };
+    }];
     return {
         restrict: 'E',
         transclude: true,
@@ -6,25 +53,7 @@ app.directive('tabset', function () {
         bindToController: true,
         controllerAs: 'tabset',
         scope: {},
-        controller: function () {
-            var self = this;
-            self.tabs = [];
-            self.addTab = function (tab) {
-                self.tabs.push(tab);
-                if (self.tabs.length === 1) {
-                    tab.selected = true;
-                }
-            }
-
-            self.selectionChanged = function (selectedTab) {
-                angular.forEach(self.tabs, function (tab) {
-                    if (tab.selected && tab !== selectedTab) {
-                        tab.selected = false;
-                    }
-                });
-                selectedTab.selected = true;
-            }
-        }
+        controller: controller
     };
 });
 
@@ -33,15 +62,12 @@ app.directive('tab', function () {
         restrict: 'E',
         require: '^tabset',
         replace: true,
-        transclude:true,
+        transclude: true,
         templateUrl: './partials/tab.html',
-        scope: {
-            title: '@',
-            id: '@'
-        },
+        scope: {},
         link: function (scope, elem, attr, tabsetCtrl) {
-            scope.selected = false;
-            tabsetCtrl.addTab(scope);
+            scope.content = elem.html();
+            tabsetCtrl.tabAdd(scope);
         }
     }
 })
